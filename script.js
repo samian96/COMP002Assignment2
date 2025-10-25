2ad20b369c1e5874896fa27861207000302ee83f
samian96 <sam.iannazzo@yahoo.com>
2025-10-24 20:14:18 -0700

    update 2 added to readme / questions 2, 5 , 6

--- script.js
+++ script.js
@@ -6,7 +6,7 @@
 console.log("Hello World!");
 // 2. Write code to concatenate three strings and output the result to the console:
 // "Hello", " ", "World!"
-console.log();
+console.log("Hello" + " " + "World!");
 // 3. Write code to output "Hello" on one line and "World!" on the next line to the console.
 // *Note: You should only have one console.log() statement - how do you add a new line in
 // a string?...
@@ -16,10 +16,10 @@ console.log("Hello\nWorld!");
 console.log (3 * 8 + 2);
 // 5. Write code to output the result of the comparison:
 // 5 is less than 10.
-
+console.log (5 < 10);
 // 6. Write code to output the result of the comparison:
 // 10 is greater than 5.
-
+console.log (10 > 5);
 // 7. Write code to output the result of the comparison:
 // "Apples" is NOT equal to "Oranges"
 

95ac3819ff3400dc3c6df2ef864ff1be867b5e4d
samian96 <sam.iannazzo@yahoo.com>
2025-10-24 20:26:38 -0700

    update 4 on readme / consolelog for 7, 8, 9 , 10

--- script.js
+++ script.js
@@ -22,11 +22,12 @@ console.log (5 < 10);
 console.log (10 > 5);
 // 7. Write code to output the result of the comparison:
 // "Apples" is NOT equal to "Oranges"
-
+console.log("Apple" != "Oranges");
 // 8. Write code to output the result of the comparison:
 // 10 * 10 is greater than 50 AND 5 * 5 is less than 50
-
+console.log(10 * 10 > 50 && 5 * 5 < 50);
 // 9. Write code to output the result of the comparison:
 // 1 + 2 is greater than 5 OR 1 * 10 is greater than 5
-
+console.log(1 + 2 > 5 || 1 * 10 > 5 );
 // 10. Write code that demonstrates an automatic type conversion as shown in Chapter 01.
+console.log(10 * null);
